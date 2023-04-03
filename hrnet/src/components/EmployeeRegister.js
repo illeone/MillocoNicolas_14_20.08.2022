2a66bd7021939da81a2f7b51847b39512515deb3
Nicolas <nicolasmilloco@hotmail.com>
2023-04-03 18:17:20 +0200

    Création du composant EmployeeRegister.js

---
+++ hrnet/src/components/EmployeeRegister.js
@@ -0,0 +1,19 @@
+const EmployeeRegister = () => {
+
+return(
+
+<div>
+    <div className="container">
+        <div className="title">
+            <h1>HRnet</h1>
+        </div>
+        <div className="create-employee"><h2>Create Employee</h2></div>
+        <div className="container-employees">
+            <a href="employee-list.html">View Current Employees</a>
+        </div>   
+    </div>
+</div>
+)
+}
+
+export default EmployeeRegister;
