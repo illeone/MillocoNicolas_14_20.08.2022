b9da3ac384444e5c2810415c4c5cd93832b287af
Nicolas <nicolasmilloco@hotmail.com>
2023-04-03 18:38:24 +0200

    Ajout du formulaire en jsx dans EmployeeRegister

--- hrnet/src/components/EmployeeRegister.js
+++ hrnet/src/components/EmployeeRegister.js
@@ -12,7 +12,56 @@ return(
             <a href="employee-list.html">View Current Employees</a>
         </div>   
     </div>
-</div>
+    <div className="form-container">
+         <form>
+             <div className='inputbox first'>
+                 <label htmlFor="first-name">First Name</label>
+                 <input type="text" id="first-name" name="firstName"  /> 
+             </div>
+             <div className='inputbox last'>
+                 <label htmlFor="last-name">Last Name</label>
+                 <input type="text" id="last-name" name="lastName" />
+             </div>
+             <div className='dates'>
+                 <div>
+                     <label htmlFor="date-of-birth">Date of Birth</label>
+                     <calendar type="date" id="date-of-birth" name="dateOfBirth" />
+                 </div>
+                 <div>
+                     <label htmlFor="start-date">Start Date</label>
+                     <calendar type="date" id="start-date" name="startDate"/>
+                 </div>
+             </div>
+
+             <fieldset className="address">
+                 <legend>Address</legend>
+                 <div>
+                     <label htmlFor="street">Street</label>
+                     <input type="text" id="street" name="street" />
+                 </div>
+                 <div>
+                     <label htmlFor="city">City</label>
+                     <input type="text" id="city" name="city" />
+                 </div>
+                 <div>
+                 <p className="state-label">State</p>
+                     <select name="state"/>
+                 </div>
+                 <div>
+                     <label htmlFor="zip-code">Zip Code</label>
+                     <input type="number" name="zipCode" />
+                 </div>
+                 <div>
+                 <p className="department-label">Department</p>
+                     <select name="department"/>
+                 </div>
+             </fieldset>
+             <div className='btn-save'>
+                 <button type="submit" text="Save" />
+             </div>
+         </form>       
+     </div> 
+ </div>
 )
 }
 
