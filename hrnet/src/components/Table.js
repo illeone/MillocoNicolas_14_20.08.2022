096e446a72ce746bac120970477c396eaf036fcb
Nicolas <nicolasmilloco@hotmail.com>
2023-04-23 15:25:22 +0200

    positionnement des boutons du tableau

--- hrnet/src/components/Table.js
+++ hrnet/src/components/Table.js
@@ -126,25 +126,27 @@ const Table = () => {
   return (
     <div className="wrapper">
       <div className="employee-table">
-        <div className="employee-table-top-left">
-          <label htmlFor="pageSize" className="search-entries">
-            Show entries:{" "}
-          </label>
-          <div className="custom-select-wrapper">
-              <DropDown
-                options={[
-                  { value: 5, label: "5" },
-                  { value: 10, label: "10" },
-                  { value: 20, label: "20" },
-                ]}
-                onChange={(event) => {
-                  setPageSize(Number(event.target.value));
-                  setCurrentPage(0);
-                }}
-                value={pageSize}
-                name="pageSize"
-                submit={false}
-              />
+        <div className="employee-table-controls">
+          <div className="employee-table-top-left">
+            <label htmlFor="pageSize" className="search-entries">
+              Show entries:{" "}
+            </label>
+            <div className="custom-select-wrapper">
+                <DropDown
+                  options={[
+                    { value: 5, label: "5" },
+                    { value: 10, label: "10" },
+                    { value: 20, label: "20" },
+                  ]}
+                  onChange={(event) => {
+                    setPageSize(Number(event.target.value));
+                    setCurrentPage(0);
+                  }}
+                  value={pageSize}
+                  name="pageSize"
+                  submit={false}
+                />
+              </div>
             </div>
           <SearchBar onSearch={handleSearch} />
         </div>
@@ -197,13 +199,13 @@ const Table = () => {
         <div className="employee-table-controls">
           <div className="employee-table-bottom-left">
             <button
-              className="employee-table-button"
+              className="employee-table-button-delete"
               onClick={handleDelete}
               style={{
                 display: selectedRows.size > 0 ? "inline-block" : "none",
               }}
             >
-              <span className="employee-table-button-content">
+              <span className="employee-table-button-delete-content">
                 <i className="fas fa-trash-alt icon"></i>
                 <span>Delete</span>
               </span>
