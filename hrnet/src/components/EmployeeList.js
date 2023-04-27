147425fba9e212f7af17fc4b0d73f80813293662
Nicolas <nicolasmilloco@hotmail.com>
2023-04-27 11:54:34 +0200

    modification graphique du header EmployeeList

--- hrnet/src/components/EmployeeList.js
+++ hrnet/src/components/EmployeeList.js
@@ -6,11 +6,13 @@ const EmployeeList = () => {
   return (
     <div className="employee-list-container">
       <div className="container">
-        <div className="title">
-          <h1>HRnet</h1>
+        <div class="block-container">
+          <div class="btn-back btn-back-1"></div>
+          <div class="btn-front creased-text">HRnet</div>
+          <div class="hover-text">Wealth Health</div>
         </div>
-        <div className="create-employee">
-          <h2>Current Employees</h2>
+        <div className="create-employee no-delay ">
+          <h2 className="create-employee-shadow">Current Employees</h2>
         </div>
         <NavLink to={"/"} style={{ textDecoration: "none" }}>
             <div className="container-employees">
