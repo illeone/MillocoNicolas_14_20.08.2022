684b1df916bf5882fd4003887acf9bd1cf6cdf12
Nicolas <nicolasmilloco@hotmail.com>
2024-02-16 12:27:26 +0100

    Intégration de localStorage dans le Context API

--- hrnet/src/EmployeeContext.js
+++ hrnet/src/EmployeeContext.js
@@ -1,18 +1,26 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const EmployeeContext = createContext();
 
 export const useEmployees = () => useContext(EmployeeContext);
 
 export const EmployeeProvider = ({ children }) => {
-  const [employees, setEmployees] = useState([]);
+  const [employees, setEmployees] = useState(() => {
+    const storedEmployees = localStorage.getItem('employees');
+    return storedEmployees ? JSON.parse(storedEmployees) : [];
+  });
 
   let employeeIdCounter = 0;
 
   const addEmployee = (employee) => {
     const newEmployee = { ...employee, id: ++employeeIdCounter };
     setEmployees(prevEmployees => [...prevEmployees, newEmployee]);
+    localStorage.setItem('employees', JSON.stringify(newEmployee));
   };  
+
+  useEffect(() => {
+    localStorage.setItem('employees', JSON.stringify(employees));
+  }, [employees]);
 
   return (
     <EmployeeContext.Provider value={{ employees,addEmployee }}>
